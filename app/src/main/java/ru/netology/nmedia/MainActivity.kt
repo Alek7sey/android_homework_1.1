8be253a71cfa1a9e0eb60bf79c98df6c69f5b108
Alexey Ashikhmin <ash60782@gmail.com>
2023-05-28 17:12:44 +0500

    rename ImageView - ImageButton

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,11 +2,16 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
 
 class  MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<ImageButton>(R.id.likes).setOnClickListener {
+            Log.d("MainActivity", "Слушатель слушает")
+        }
 
     }
 }
