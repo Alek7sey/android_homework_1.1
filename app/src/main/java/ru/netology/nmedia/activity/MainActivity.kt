015ba5a1a746bb2410763da74da462ee794e052b
Alexey Ashikhmin <ash60782@gmail.com>
2023-06-12 22:33:32 +0500

    add CRUD, cancel post editing

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -2,32 +2,89 @@ package ru.netology.nmedia.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
+import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.OnInteractionListener
 import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.utils.AndroidUtils
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    val viewModel: PostViewModel by viewModels()
+
+    private val interactionListener: OnInteractionListener = object : OnInteractionListener {
+        override fun onLike(post: Post) {
+            viewModel.likeById(post.id)
+        }
+
+        override fun onShare(post: Post) {
+            viewModel.shareById(post.id)
+        }
+
+        override fun onViews(post: Post) {
+            viewModel.viewById(post.id)
+        }
+
+        override fun onEdit(post: Post) {
+            viewModel.edit(post)
+        }
+
+        override fun onRemove(post: Post) {
+            viewModel.removeById(post.id)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val viewModel: PostViewModel by viewModels()
-        val adapter = PostsAdapter({
-            viewModel.likeById(it.id)
-        },
-            {
-                viewModel.shareById(it.id)
-            },
-            {
-                viewModel.viewById(it.id)
-            })
+        val adapter = PostsAdapter(interactionListener)
         binding.list.adapter = adapter
         viewModel.data.observe(this) { posts ->
             adapter.submitList(posts)
+        }
+
+        viewModel.edited.observe(this) { post ->
+            if (post.id != 0L) {
+                with(binding.contentEdit) {
+                    binding.group.visibility = View.VISIBLE
+                    requestFocus()
+                    setText(post.content)
+                }
+            }
+        }
+
+        binding.save.setOnClickListener {
+            with(binding.contentEdit) {
+                if (text.isNullOrBlank()) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        context.getString(R.string.error_empty_content),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    viewModel.changeContent(text.toString())
+                    viewModel.save()
+                    setText("")
+                    clearFocus()
+                    AndroidUtils.hideKeyboard(this)
+                    binding.group.visibility = View.GONE
+                }
+            }
+        }
+        binding.cancel.setOnClickListener {
+            with(binding.contentEdit) {
+                setText("")
+                clearFocus()
+                AndroidUtils.hideKeyboard(this)
+                binding.group.visibility = View.GONE
+            }
         }
     }
 }
--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -1,13 +1,46 @@
 package ru.netology.nmedia.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
 import ru.netology.nmedia.repository.PostRepositoryInMemoryImp
 
+private val empty = Post(
+    id = 0,
+    content = "",
+    author = "",
+    published = "",
+    likedByMe = false
+)
+
 class PostViewModel : ViewModel() {
+
     private val repository: PostRepository = PostRepositoryInMemoryImp()
     val data = repository.getAll()
+    val edited = MutableLiveData(empty)
+
     fun likeById(id: Long) = repository.likeById(id)
     fun shareById(id: Long) = repository.shareById(id)
     fun viewById(id: Long) = repository.viewById(id)
+
+    fun save() {
+        edited.value?.let {
+            repository.save(it)
+        }
+        edited.value = empty
+    }
+
+    fun edit(post: Post) {
+        edited.value = post
+    }
+
+    fun changeContent(content: String) {
+        val text = content.trim()
+        if (edited.value?.content != text) {
+            edited.value = edited.value?.copy(content = text)
+        }
+    }
+
+    fun removeById(id: Long) = repository.removeById(id)
 }
