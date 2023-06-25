d67b3d3da43cbfa5fe389465529b8977478d3ca4
Alexey Ashikhmin <ash60782@gmail.com>
2023-06-25 19:11:03 +0500

    add editing in a new Activity

---
+++ app/src/main/java/ru/netology/nmedia/activity/NewPostActivity.kt
@@ -0,0 +1,31 @@
+package ru.netology.nmedia.activity
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.netology.nmedia.databinding.ActivityNewPostBinding
+
+class NewPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityNewPostBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.editText.requestFocus()
+        val editContent = intent.getStringExtra(Intent.EXTRA_TEXT)
+        if (editContent != "null") {
+            binding.editText.setText(editContent)
+        }
+        binding.btnOk.setOnClickListener {
+            if(binding.editText.text.isBlank()) {
+                setResult(Activity.RESULT_CANCELED, intent)
+            } else {
+                val content = binding.editText.text.toString()
+                intent.putExtra(Intent.EXTRA_TEXT, content)
+                setResult(Activity.RESULT_OK, intent)
+            }
+            finish()
+        }
+
+    }
+}
